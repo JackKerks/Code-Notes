49b3b9b47b0e1b1082b11cd06a771a54a4531df2
JackKerks <jack.kerklaan19@alumni.loyola.ca>
2017-12-15 09:22:58 -0500

    Add files via upload

---
+++ Class Schedule.js
@@ -0,0 +1,62 @@
+switch var {
+	case "French":
+	document.getElementById("bestclass").style.background = white;
+		break
+		
+
+		
+}
+
+var text;
+var CompProg = document.getElementById("bestclass").style.background = white;
+
+switch(bestclass) {
+    case "CompProg":
+        background = white;
+        break;
+
+
+switch 	{
+	
+		case "English":
+		break;
+	case French:
+         break;
+    
+	case English:
+        break;
+		
+	case Math:
+        break;
+		
+    case Science:
+        break;
+		
+	case Architecture:
+        break;
+		
+    case PE:
+        break;
+		
+	case Religion:
+        break;
+		
+    case CompProg:
+        break;
+		
+	case History:
+        break;
+		
+    case HomeRoom:
+        break;
+		
+	case Advisors:
+        break;
+		
+    case Speech:
+        break;
+	
+	case L.A.:
+
+        break;
+}
